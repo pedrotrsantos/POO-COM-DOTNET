2ad6239d599ac7812ee07c5fc4dee17e2368c935
PedJump <pthyago212@gmail.com>
2024-02-20 18:20:40 -0300

    Exercicio Calculo de IMC

---
+++ 04ExercicioIMC/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _04ExercicioIMC
+{
+    internal class Pessoa
+    {
+        public double peso;
+        public double altura;
+
+        public double imc()
+        {
+            return peso / (altura * altura);
+        }
+
+        public string resultado(double imc)
+        {
+           if (imc <  18.5)
+            {
+                return "Abaixo do peso";
+            }else if( imc > 18.5 &&  imc < 25)
+            {
+                return "Peso normal";
+            }
+            else if (imc > 25 && imc < 30)
+            {
+                return "Acima do peso";
+            }
+            else if (imc > 30 && imc < 35)
+            {
+                return "Obesidade I";
+            }
+            else if (imc > 35 && imc < 40)
+            {
+                return "Obesidade II";
+            }
+            else if (imc >= 40)
+            {
+                return "Obesidade III";
+            }
+
+            return null;
+        }
+
+        public void mensagem()
+        {
+            double obterImc = imc();
+            string obterResultado  = resultado(obterImc);
+            Console.WriteLine ("O seu IMC foi de "+ obterImc + " você está com "  + obterResultado);
+           
+        }
+
+    }
+}
+
---
+++ 04ExercicioIMC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04ExercicioIMC
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("CALCULADOR DE IMC \n \n");
+
+            Console.WriteLine("Digite o seu peso: ");
+
+            pessoa.peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o sua altura em metros:  ");
+            var altura = Console.ReadLine().Replace(".", ",");
+            pessoa.altura = double.Parse(altura);
+
+            pessoa.mensagem();
+        }
+    }
+
+}
+
