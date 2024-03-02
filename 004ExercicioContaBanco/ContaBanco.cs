15b1578499369161aabd1100b90da8c2e7e5f23b
PedJump <pthyago212@gmail.com>
2024-03-01 21:48:55 -0300

    Exercicio de Conta de banco  \ saque \ deposito

---
+++ 004ExercicioContaBanco/ContaBanco.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004ExercicioContaBanco
+{
+    public class ContaBanco
+    {
+        public int NumConta { get; set; }
+        protected TipoConta Tipo { get; set; }
+        private string Dono { get; set; }
+        private double Saldo { get; set; }
+        private bool Status { get; set; }
+        private double Limite { get; set; }
+
+        public ContaBanco()
+        {
+            Saldo = 0;
+            Status = false;
+           
+        }
+
+
+        public void AbrirConta(TipoConta tipo, string dono, double limite)
+        {
+
+            var GerarConta = new Random();
+
+            NumConta = GerarConta.Next(10000000, 99999999);
+            Tipo = tipo;
+            Dono = dono;
+            Limite = limite;
+            Status = true;
+        }
+
+        public void FecharConta()
+        {
+            if (Status == false)
+            {
+                Console.WriteLine("ERRO, ESSA CONTA NÁO ESTÁ ATIVA!");
+            }
+            else if (Saldo > 0)
+            {
+                Console.WriteLine("ERRO, TEM SALDO NA CONTA!");
+            }
+            else if (Saldo < 0)
+            {
+                Console.WriteLine("ERRO, TEM SALDO NEGATIVO EM SUA CONTA!");
+            }
+            else if (Saldo == 0)
+            {
+                Status = false;
+                Console.WriteLine("A CONTA FOI FECHADA COM SUCESSO");
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (Status == false)
+            {
+                Console.WriteLine("ERRO, ESSA CONTA NÁO ESTÁ ATIVA!");
+            }
+            else if (valor <= 0)
+            {
+                Console.WriteLine("ERRO, 'VALOR DE DEPÓSITO INVALIDO");
+            }
+            else
+            {
+                Saldo += valor;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (Status == false)
+            {
+                Console.WriteLine("ERRO, ESSA CONTA NÁO ESTÁ ATIVA!");
+            } else if (valor <= 0 )
+            {
+                Console.WriteLine("ERRO, VALOR DE SAQUE INVÁLIDO!");
+            }else if (valor > (Saldo + Limite))
+            {
+                Console.WriteLine("ERRO, O VALOR EXTRAPOLOU O SEU LIMITE DE SAQUE");
+            }
+            else
+            {
+                Saldo -= valor;
+                Console.WriteLine($"SAQUE DE R$ {valor} realizado com sucesso");
+            }
+        }
+        public void Anuidade()
+        {
+            if (Tipo == TipoConta.Cc)
+            {
+                Saldo -= 12;
+            } else
+            {
+                Saldo -= 20;
+            }
+        }
+
+        public void Extrato()
+        {
+            Console.WriteLine($@"
+Nome: {Dono} 
+Conta: {NumConta} 
+Tipo: {Tipo} 
+Status: { Status} 
+Saldo: R$ {Saldo}
+");
+
+
+        }
+
+
+
+
+    }
+    public enum TipoConta
+    {
+        Cc = 1,
+        Cp = 2
+    }
+}
---
+++ 004ExercicioContaBanco/Program.cs
@@ -0,0 +1,35 @@
+﻿using _004ExercicioContaBanco;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBanco c1 = new ContaBanco();
+        c1.AbrirConta(TipoConta.Cc, "Tonin do Posto", 100);
+        c1.Depositar(100);
+        c1.Extrato();
+        c1.Sacar(20);
+        c1.Extrato();
+        c1.Anuidade();
+        c1.Extrato();
+
+        ContaBanco c2 = new ContaBanco();
+        c2.AbrirConta(TipoConta.Cp, "Jorgin Mecanico", 0);
+        c2.Depositar(500);
+        c2.Extrato();
+        c2.Sacar(600);
+        c2.Extrato();
+        c2.Anuidade();
+        c2.Extrato();
+
+        ContaBanco c3 = new ContaBanco();
+        c3.AbrirConta(TipoConta.Cp, "Seu Junior", 100);
+        c3.Depositar(50);
+        c3.Extrato();
+        c3.Sacar(150);
+        c3.Extrato();
+        c3.Anuidade();
+        c3.Extrato();
+    }
+}
