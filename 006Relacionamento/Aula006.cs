6200e633a4daf81a8348eb8c59e68e2735fb11fe
PedJump <pthyago212@gmail.com>
2024-03-13 23:01:17 -0300

    Criação da classe Lutador

---
+++ 006Relacionamento/Aula006.cs
@@ -0,0 +1,45 @@
+﻿using _006Relacionamento;
+using System;
+
+class Aula006
+{
+    static void Main(string[] args)
+    {
+        Lutador[] l = new Lutador[6];
+
+        l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+        l[3] = new Lutador("KanguruJack", "Australia", 28, 1.93, 81.6, 13, 0, 2);
+        l[4] = new Lutador("Popó", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+        l[5] = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+
+
+        l[0].Apresentar();
+        l[0].GanharLuta();
+        l[0].Status();
+
+        l[1].Apresentar();
+        l[1].GanharLuta();
+        l[1].Status();
+
+        l[2].Apresentar();
+        l[2].GanharLuta();
+        l[2].Status();
+
+        l[3].Apresentar();
+        l[3].GanharLuta();
+        l[3].Status();
+
+        l[4].Apresentar();
+        l[4].GanharLuta();
+        l[4].Status();
+
+        l[5].Apresentar();
+        l[5].GanharLuta();
+        l[5].Status();
+
+
+
+    }
+}
---
+++ 006Relacionamento/Lutador.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006Relacionamento
+{
+    class Lutador
+    {
+        private string Nome { get; set; }
+        private string Nacionalidade { get; set; }
+        private int Idade { get; set; }
+        private double Altura { get; set; }
+        private double Peso { get; set; }
+        private string Categoria { get; set; }
+        private int Vitorias { get; set; }
+        private int Derrotas { get; set; }
+        private int Empates { get; set; }
+
+        public Lutador(string nome, string nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates)
+        {
+            Nome = nome;
+            Nacionalidade = nacionalidade;
+            Idade = idade;
+            Altura = altura;
+            Peso = peso;
+            Vitorias = vitorias;
+            Derrotas = derrotas;
+            Empates = empates;
+            Categoria = DefinirCategoria();
+        }
+
+        public string DefinirCategoria()
+        {
+            if (Peso < 52.2)
+            {
+                return Categoria = "Inválido";
+            }
+            else if (Peso <= 70.3)
+            {
+                return Categoria = "Leve";
+            }
+            else if (Peso <= 83.9)
+            {
+                return Categoria = "Médio";
+            }
+            else if (Peso <= 120.2)
+            {
+                return Categoria = "Pesado";
+            }
+            else {
+                return Categoria = "Inválida";
+            }
+        }
+        public void Apresentar() {
+            Console.WriteLine("----- APRESENTAÇÃO ----- ");
+            Console.WriteLine($"Lutador : {Nome}");
+            Console.WriteLine($"Origem: {Nacionalidade}");
+            Console.WriteLine($"Com {Idade} anos");
+            Console.WriteLine($"Altura {Altura}m e pesando {Peso} Kg");
+            Console.WriteLine($"Ganhou: {Vitorias}");
+            Console.WriteLine($"Perdeu: {Derrotas}");
+            Console.WriteLine($"Empatou: {Empates}  \n");
+        }
+        public void Status()
+        {
+            Console.WriteLine("----- STATUS -----");
+               Console.WriteLine($"É um peso {Categoria}");
+            Console.WriteLine($"Com {Vitorias} vitórias, {Derrotas} derrotas e {Empates} empates  \n ");
+        }
+        public void GanharLuta()
+        {
+            Vitorias += 1;
+        }
+
+        public void PerderLuta()
+        {
+            Derrotas += 1;
+        }
+        public void EmpatarLuta()
+        {
+            Empates += 1;
+        }
+    }
+}
